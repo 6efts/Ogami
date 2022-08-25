4b64e102a57a22bc25ccf393f5d413a162771f9d
James Anuncio <anunciojames14@gmail.com>
2022-08-25 13:40:25 +0800

    Feat: header update

--- src/components/global component/Header.js
+++ src/components/global component/Header.js
@@ -1,34 +1,66 @@
-import React from 'react'
-import {FaEnvelope, FaPhoneAlt}  from "react-icons/fa";
+import React from "react";
+import { FaEnvelope, FaPhoneAlt, FaFacebookF, FaTwitter, FaLinkedin, FaPinterestP, FaAngleDown, FaUser } from "react-icons/fa";
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <header className='Header'>
-        <div className='Header__first'>
+    <header className="Header">
+      <div className="Header__first">
+        <div className="Header__first--left">
           <ul>
+            <FaEnvelope size={17} color="#252525" />
+
             <li>
-              <FaEnvelope 
-              size={20}
-              color = "#252525"
-              />
               <span>info@ogami.com</span>
             </li>
-            <li> | </li>
+
+            <li className="divider"> | </li>
+
+            <FaPhoneAlt size={17} />
             <li>
-              <FaPhoneAlt
-               size={20}
-              />
-              <span>info@ogami.com</span>
+              <span>+65 11.188.888</span>
             </li>
           </ul>
         </div>
+        <div className="Header__first--right">
+          <ul>
+            <li> <FaFacebookF /> </li>
+            <li> <FaTwitter /> </li>
+            <li> <FaLinkedin /> </li>
+            <li> <FaPinterestP /> </li>
+          </ul>
+         
+          <span className="divider">|</span>
 
-        <div className='Header__second'>
+          <span className="country"> 
+            <img src="https://ogami-react.vercel.app/assets/images/header/flag-usa.png"  alt="flag"/>
+            <p>English</p>
+
+            <span className="arrow">
+              <FaAngleDown  color="#252525" size={20}/>
+            </span>
+
+          </span>
+
+          <span className="divider">|</span>
+
+          <span className="user"> 
+            <FaUser />
+            <p>Login</p>
+          </span>
 
         </div>
-    </header>
-  )
-}
+      </div>
 
-export default Header
+      <div className="Header__second">
+      
+        <img src="https://ogami-react.vercel.app/assets/images/logo.png" alt="logo" />
+
+        
+
+      </div>
+    </header>
+  );
+};
+
+export default Header;
