3198a04f27c017e9734df3e776b60735a95be203
Jarren <vince.dehonor1@gmail.com>
2022-08-27 14:43:14 +0800

    Feat: Footer

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../components/global component/Footer'
 import Header from '../components/global component/Header'
 
 const Home = () => {
   return (
     <div>
-        <Header />
+      <Header />
+      <Footer />
     </div>
   )
 }
