725b283a3c8297eb6389de12f0b5264fc8567e02
James Anuncio <anunciojames14@gmail.com>
2022-09-02 11:30:13 +0800

    Feat: added timer

--- src/components/home component/Deals.js
+++ src/components/home component/Deals.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import './Deals.scss';
+import React from "react";
+import "./Deals.scss";
+import Timer from "./Timer";
 
 const Deals = () => {
   return (
-    <div className='Deals'>
-        <div className='Deals__content'>
-            <h2>Deal Of The Week</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, reprehenderit ut.</p>
-        </div>
-        <div className='Deals__image'>
-            <img src="https://ogami-react.vercel.app/assets/images/sections/dale-of-week/two/1.png" alt="deals" />
-         </div>
-    </div>
-  )
-}
+    <div className="Deals">
+      <div className="Deals__content">
+        <h2>Deal Of The Week</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+          reprehenderit ut.
+        </p>
 
-export default Deals
+        <div className="Deals__content--timer">
+          <Timer />
+        </div>
+
+        <button>Shop Now</button>
+
+      </div>
+      <div className="Deals__image">
+        <img
+          src="https://ogami-react.vercel.app/assets/images/sections/dale-of-week/two/1.png"
+          alt="deals"
+        />
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Deals;
---
+++ src/components/home component/Timer.js
@@ -0,0 +1,76 @@
+import React, {useState, useRef, useEffect}   from 'react';
+import './Timer.scss';
+import {format} from 'date-fns';
+
+
+const Timer = () => {
+    const [timerDays, setTimerDays] = useState('00');
+    const [timerHours, setTimerHours] = useState('00');
+    const [timerMinutes, setTimerMinutes] = useState('00');
+    const [timerSeconds, setTimerSeconds] = useState('00');
+
+    let interval = useRef();
+
+    const startTimer = () => {
+        const countdownDate = new Date('September 6, 2022').getTime();
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 *60 * 60)));
+            const minutes =Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+
+
+
+            if (distance < 0){
+                clearInterval(interval.current);
+            }else{
+                setTimerDays(days);
+                setTimerHours(hours);
+                setTimerMinutes(minutes);
+                setTimerSeconds(seconds);
+            }
+            
+        }, 1000)  
+        
+    }
+
+    useEffect(() => {
+        startTimer();
+        console.log(timerDays);
+        return () => {
+            clearInterval(interval.current);
+        }
+    })
+
+
+  return (
+    <div className='Timer'>
+        <section className='Timer__content'>
+            <p>{timerDays}</p>
+            <h6>DAYS</h6>
+        </section>
+
+        <section className='Timer__content'>
+            <p>{timerHours}</p>
+            <h6>HR</h6>
+        </section>
+
+        <section className='Timer__content'>
+            <p>{timerMinutes}</p>
+            <h6>MIN</h6>
+        </section>
+
+        <section className='Timer__content'>
+            <p>{timerSeconds}</p>
+            <h6>SEC</h6>
+        </section>
+
+    </div>
+  )
+}
+
+export default Timer
